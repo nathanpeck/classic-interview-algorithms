4ee17f4afc04cef7d21cc4204fa6be4c2feeddfc
Nathan Peck <nathan@airtime.com>
2016-11-03 13:59:53 -0400

    Create is-pangram.js

---
+++ is-pangram.js
@@ -0,0 +1,25 @@
+// Function to check if a string is a pangram (sentence containing all the letters of
+// the alphabet).
+
+function isPanagram(string) {
+  var alphabet = {};
+
+  for (var char = 0; char < string.length; char++) {
+    var currentChar = string[char].toLowerCase();
+    
+    if (currentChar.match(/^[A-z]+$/)) {
+      alphabet[currentChar] = true;
+    }
+  }
+  
+  console.log(alphabet);
+
+  if (Object.keys(alphabet).length == 26) {
+    return true;
+  }
+  
+  return false;
+}
+
+console.log(isPanagram('Pack my box with five dozen liquor jugs'));
+console.log(isPanagram('not a panagram'));
